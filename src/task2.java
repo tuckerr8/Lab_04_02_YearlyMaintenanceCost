5e8dd2595f4e82543e912595bc1a4efd260f9743
tuckerr8 <rodzell@outlook.com>
2024-02-04 19:25:23 -0500

    Initial commit

---
+++ src/task2.java
@@ -0,0 +1,13 @@
+public class task2 {
+    public static void main(String[] args) {
+        int springMaintenance = 250;
+        int summerMaintenance = 300;
+        int fallMaintenance = 210;
+        int winterMaintenance = 350;
+
+        int totalCost = springMaintenance + summerMaintenance + fallMaintenance + winterMaintenance;
+
+        System.out.println("The yearly combined total for all seasons is: " + "$" + totalCost);
+
+    }
+}
